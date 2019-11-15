13b3622b53a82d0a9c2d3926f632f774b612e65d
Jamison Bunge <JamisonBunge@outlook.com>
2019-11-15 11:46:01 -0500

    cells black out based on the year, made cells clickable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './index.css';
 import Collection from './Components/Collection'
 
-function App() {
-    return (
-        <div className="App">
-            <Collection />
-        </div>
-    );
+
+
+
+function setValue() {
+    let cell = document.getElementById('row-3-col-1');
+    console.log(cell)
+    cell.classList.add("black")
+}
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        let date = new Date();
+        this.state = {
+            "year": date.getFullYear(),
+            "date": date
+        }
+    }
+
+    componentDidMount() {
+        let blackOut = ['row-31-col-4', 'row-31-col-6', 'row-31-col-9', 'row-31-col-11', 'row-30-col-2', 'row-31-col-2'];
+        let year = this.year;
+        if (!(year % 100 === 0 ? year % 400 === 0 : year % 4 === 0)) blackOut.push('row-29-col-2');
+
+        for (let day of blackOut) {
+            let cell = document.getElementById(day);
+            cell.classList.add("black");
+            console.log(cell);
+            console.log(this.state);
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="App" >
+                <button type="input" onClick={setValue}>######################</button>
+                <Collection />
+            </div>
+        );
+    }
 }
 
 export default App;
--- frontend/src/Components/Collection.js
+++ frontend/src/Components/Collection.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ReactDataGrid from 'react-data-grid';
 
 let months = [
     { id: 1, "name": "January" },
@@ -16,32 +15,82 @@ let months = [
     { id: 12, "name": "December" },
 ];
 
-let buildRow = (rowId) => {
 
-    let cells = []
-    cells.push(<td className={`rowNum-${rowId % 2}`}>{rowId}</td>)
 
-    for (let col = 1; col < 13; col++) {
-        cells.push(<td id={`row-${rowId}-col-${col}`} className={`rowNum-${rowId % 2}`}></td>)
+
+
+
+let getStateContainer = () => {
+
+    let months = [];
+    let days = [];
+
+    for (let month = 0; month < 13; month++) {
+        for (let day = 0; day < 32; day++) {
+            days.push({ "val": 0, "class": "default" });
+        }
+        days = [];
+        months.push(days);
     }
-    return cells;
+
+    return months;
 }
 
-let buildHeader = () => {
-
-    let header = [];
-    header.push(<th>Day #</th>)
-
-    for (let month of months) {
-        header.push(<th id={`month-${month.id}`}>{month.name}</th>)
-    }
-    return header;
-}
 
 class Collection extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            cells: getStateContainer()
+        }
+    }
+
+    componentDidMount(cellState) {
+
+    }
+
+
+    logEvent(e) {
+
+        let cell = (document.getElementById(e.target.id))
+        cell.classList.add("green")
+
+
+        // let decomposedId = id.splice('-');
+        // let cell = this.state.cells[1][1];
+        console.log("hello")
+
+    }
+
+
+    buildRow(rowId) {
+
+        let cells = []
+        cells.push(<td className={`rowNum-${rowId % 2}`}>{rowId}</td>)
+
+        for (let col = 1; col < 13; col++) {
+            cells.push(<td id={`row-${rowId}-col-${col}`}
+                className={`rowNum-${rowId % 2}`}
+                onClick={this.logEvent} ></td>)
+        }
+        return cells;
+    }
+
+
+    //logEvent(this.state.cells[col][rowId])
+
+    //ThIS IS
+    buildHeader() {
+
+        console.log(Date())
+        let header = [];
+        header.push(<th>Day #</th>)
+
+        for (let month of months) {
+            header.push(<th id={`month-${month.id}`} >{month.name}</th>)
+        }
+        return header;
     }
 
     render() {
@@ -49,38 +98,40 @@ class Collection extends Component {
         return (
             <div className="collection">
                 <table id="grid">
-                    <tr id="Months">{buildHeader()}</tr>
-                    <tr id="1">{buildRow(1)}</tr>
-                    <tr id="1">{buildRow(2)}</tr>
-                    <tr id="2">{buildRow(3)}</tr>
-                    <tr id="3">{buildRow(4)}</tr>
-                    <tr id="4">{buildRow(5)}</tr>
-                    <tr id="5">{buildRow(6)}</tr>
-                    <tr id="6">{buildRow(7)}</tr>
-                    <tr id="7">{buildRow(8)}</tr>
-                    <tr id="8">{buildRow(9)}</tr>
-                    <tr id="9">{buildRow(10)}</tr>
-                    <tr id="10">{buildRow(11)}</tr>
-                    <tr id="11">{buildRow(12)}</tr>
-                    <tr id="12">{buildRow(13)}</tr>
-                    <tr id="12">{buildRow(14)}</tr>
-                    <tr id="12">{buildRow(15)}</tr>
-                    <tr id="12">{buildRow(16)}</tr>
-                    <tr id="12">{buildRow(17)}</tr>
-                    <tr id="12">{buildRow(18)}</tr>
-                    <tr id="12">{buildRow(19)}</tr>
-                    <tr id="12">{buildRow(20)}</tr>
-                    <tr id="12">{buildRow(21)}</tr>
-                    <tr id="12">{buildRow(22)}</tr>
-                    <tr id="12">{buildRow(23)}</tr>
-                    <tr id="12">{buildRow(24)}</tr>
-                    <tr id="12">{buildRow(25)}</tr>
-                    <tr id="12">{buildRow(26)}</tr>
-                    <tr id="12">{buildRow(27)}</tr>
-                    <tr id="12">{buildRow(28)}</tr>
-                    <tr id="12">{buildRow(29)}</tr>
-                    <tr id="12">{buildRow(30)}</tr>
-                    <tr id="12">{buildRow(31)}</tr>
+                    <tbody>
+                        <tr id="Months">{this.buildHeader()}</tr>
+                        <tr id="1">{this.buildRow(1)}</tr>
+                        <tr id="1">{this.buildRow(2)}</tr>
+                        <tr id="2">{this.buildRow(3)}</tr>
+                        <tr id="3">{this.buildRow(4)}</tr>
+                        <tr id="4">{this.buildRow(5)}</tr>
+                        <tr id="5">{this.buildRow(6)}</tr>
+                        <tr id="6">{this.buildRow(7)}</tr>
+                        <tr id="7">{this.buildRow(8)}</tr>
+                        <tr id="8">{this.buildRow(9)}</tr>
+                        <tr id="9">{this.buildRow(10)}</tr>
+                        <tr id="10">{this.buildRow(11)}</tr>
+                        <tr id="11">{this.buildRow(12)}</tr>
+                        <tr id="12">{this.buildRow(13)}</tr>
+                        <tr id="12">{this.buildRow(14)}</tr>
+                        <tr id="12">{this.buildRow(15)}</tr>
+                        <tr id="12">{this.buildRow(16)}</tr>
+                        <tr id="12">{this.buildRow(17)}</tr>
+                        <tr id="12">{this.buildRow(18)}</tr>
+                        <tr id="12">{this.buildRow(19)}</tr>
+                        <tr id="12">{this.buildRow(20)}</tr>
+                        <tr id="12">{this.buildRow(21)}</tr>
+                        <tr id="12">{this.buildRow(22)}</tr>
+                        <tr id="12">{this.buildRow(23)}</tr>
+                        <tr id="12">{this.buildRow(24)}</tr>
+                        <tr id="12">{this.buildRow(25)}</tr>
+                        <tr id="12">{this.buildRow(26)}</tr>
+                        <tr id="12">{this.buildRow(27)}</tr>
+                        <tr id="12">{this.buildRow(28)}</tr>
+                        <tr id="12">{this.buildRow(29)}</tr>
+                        <tr id="12">{this.buildRow(30)}</tr>
+                        <tr id="12">{this.buildRow(31)}</tr>
+                    </tbody>
                 </table>
             </div>
         );
